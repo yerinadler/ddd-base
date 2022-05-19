073deb77d31acdb82e84874ef5e08009ffc4d723
Yerin Adler <yerin.adler@gmail.com>
2022-05-20 00:18:26 +0700

    Mod Event

--- src/lib/event.interface.ts
+++ src/lib/event.interface.ts
@@ -3,5 +3,6 @@ import { IMessage } from './message.interface';
 export interface IEvent extends IMessage {
   eventName: string;
   aggregateName: string;
+  aggregateId: string;
   version?: number;
 }
